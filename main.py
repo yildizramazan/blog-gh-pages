9b5b67d56f4d51059bbec0afa08fb489c2cfa85d
Ramazan Yildiz <ry.yildizramazan@gmail.com>
2023-08-17 15:54:07 +0300

    sending email is now working.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from requests import get
+import smtplib
 
 app = Flask(__name__)
 response = get(url="https://api.npoint.io/25e0d288f42fe7019727").json()
-
+OWN_EMAIL = "YOUR EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 
 
 
@@ -15,9 +17,25 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        send_email(name, email, phone, message)
     return render_template('contact.html')
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -30,5 +48,9 @@ def show_post(index):
 
 
 
+
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
